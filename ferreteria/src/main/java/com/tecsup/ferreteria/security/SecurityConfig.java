829d5c6d559f64674b14a34336cefb26f501f068
sergio15dev <sergio15.dev@gmail.com>
2023-12-01 23:29:42 -0500

    security config

--- ferreteria/src/main/java/com/tecsup/ferreteria/security/SecurityConfig.java
+++ ferreteria/src/main/java/com/tecsup/ferreteria/security/SecurityConfig.java
@@ -1,8 +1,86 @@
 package com.tecsup.ferreteria.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class SecurityConfig implements WebMvcConfigurer {
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final UserService userService;
+
+    @Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+		auth.setUserDetailsService(usuarioServicio);
+		auth.setPasswordEncoder(passwordEncoder());
+		return auth;
+	}
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf ->
+                    csrf
+                    .disable()
+                )
+                .authorizeHttpRequests(authRequest -> 
+                    authRequest
+                        .requestMatchers("/registro**").permitAll()
+                        .requestMatchers("/js/**").permitAll()
+                        .requestMatchers("/css/**").permitAll()
+                        .requestMatchers("/img/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                // .sessionManagement(sessionManager -> 
+                //     sessionManager
+                //         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                // )
+                .formLogin(formLogin ->
+                    formLogin
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(logout ->
+                    logout
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                )
+                // .authenticationProvider(null)
+                // .addFilterBefore(null, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+	protected void configure(AuthenticationManagerBuilder auth)  {
+		auth.authenticationProvider(authenticationProvider());
+	}
+    
+	
     
 }
